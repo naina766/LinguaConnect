94a58b5ab20a4050453e04ba5aa89ea728862d19
naina766 <nainavarshney842@gmail.com>
2025-11-17 21:51:02 +0530

    Remove OPTIONS method from CORS configuration

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
